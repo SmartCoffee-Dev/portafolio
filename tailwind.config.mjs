50bffe15a49e3c5c44061fe6dfbb2d13728479b2
SmartCoffee-Dev <smartcoffee.dev@gmail.com>
2024-04-19 21:49:55 -0600

    Font => Using the museo moderno font just for the headers and the montserrat for the content.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'sans': ['Montserrat Variable', 'system-ui', 'sans-serif'],
+				'headers': ['MuseoModerno Variable', 'system-ui', 'sans-serif']
+			}
+		},
 	},
 	plugins: [],
 }
